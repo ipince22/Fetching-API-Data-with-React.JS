591a8f7ecc9bc801e14714107e8b831c86c93231
Daniel Ipince <ipince22@gmail.com>
2020-02-15 01:28:20 +0000

    02.14.20

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,22 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 export function Home() {
+	const [people, setPeople] = useState([]);
+	useEffect(() => {
+		fetch("https://swapi.co/api/people/")
+			.then(resp => resp.json())
+			.then(data => {
+				console.log(data);
+				setPeople(data;
+			});
+	}, []);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			{people.map((item, index) => {
+				return <div key={index}>{item}</div>;
+			})}
+		</>
 	);
 }
