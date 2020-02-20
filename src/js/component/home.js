b0a36450e3dcc7e7a0e963bc3376315478a6966d
Daniel Ipince <ipince22@gmail.com>
2020-02-20 01:53:02 +0000

    api react

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,20 +3,33 @@ import React, { useState, useEffect } from "react";
 //create your first component
 export function Home() {
 	const [people, setPeople] = useState([]);
+	const [movies, setMovies] = useState([]);
+
 	useEffect(() => {
 		fetch("https://swapi.co/api/people/")
 			.then(resp => resp.json())
 			.then(data => {
+				setPeople(data.results);
 				console.log(data);
-				setPeople(data;
 			});
 	}, []);
 
 	return (
 		<>
-			{people.map((item, index) => {
-				return <div key={index}>{item}</div>;
-			})}
+			<div className="list-group">
+				{people.map((item, index) => {
+					return (
+						<div
+							onClick={() => {
+								alert(item.films);
+							}}
+							className="list-group-item list-group-item-action"
+							key={index}>
+							{item.name}
+						</div>
+					);
+				})}
+			</div>
 		</>
 	);
 }
